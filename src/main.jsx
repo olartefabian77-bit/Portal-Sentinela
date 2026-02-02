cf4653e24d0169acdbe99588fc11057e0824ea4a
olartefabian77-bit <olartefabian77@gmail.com>
2026-02-01 23:59:49 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import Login from './pages/Login.jsx';
 import Panel from './pages/Panel.jsx';
 import PrivateRoute from './routes/PrivateRoute.jsx';
 import './index.css'; // si usas Tailwind o tus estilos
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
